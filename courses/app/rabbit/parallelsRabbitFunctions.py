2d4313ff8481303b2494859751c8f62e543be3af
legn0 <103703190+legn0@users.noreply.github.com>
2024-10-09 01:30:10 -0300

    added basic rabbitmq

---
+++ courses/app/rabbit/parallelsRabbitFunctions.py
@@ -0,0 +1,58 @@
+import pika as pk
+import json
+from pika.adapters.blocking_connection import BlockingChannel
+
+def publishNewParallel(course_id: int, course_name: str, parallel_id: int, parallel_number: int, limite_cupo: int, jornada: int, campus_sede: int,
+                        channel: BlockingChannel):
+    body =  {
+        "curso" : {
+            "id" : course_id,
+            "name": course_name
+        },
+        "numero": parallel_number,
+        "limite_cupo": limite_cupo,
+        "jornada": jornada,
+        "campus_sede": campus_sede
+    }
+
+    channel.basic_publish(exchange="courses",
+                          routing_key=f"parallel.{parallel_id}.created",
+                          body=json.dumps(body)
+                          )
+
+
+def publishUpdatedParallel( channel: BlockingChannel,
+                            course_id: int, course_name: str,
+                            parallel_id: int, parallel_number: int = None,
+                            limite_cupo: int = None, jornada: int = None, campus_sede: int = None):
+
+    body ={
+        "curso" : {
+            "id" : course_id,
+            "name": course_name}
+        }
+    if parallel_number != None: body["numero"] = parallel_number
+    if limite_cupo != None: body["limite_cupo"] = limite_cupo
+    if jornada != None: body["jornada"] = jornada
+    if campus_sede != None: body["campus_sede"] = campus_sede
+
+    channel.basic_publish(
+        exchange="courses",
+        routing_key=f"parallel.{parallel_id}.updated",
+        body=json.dumps(body)
+    )
+
+def publishDeletedParallel(channel: BlockingChannel,
+                           parallel_id: int, course_id: int, course_name: str):
+    
+    body = {
+        "curso" : {
+            "id" : course_id,
+            "name": course_name}
+        }
+
+    channel.basic_publish(
+        exchange="courses",
+        routing_key=f"parallel.{parallel_id}.deleted",
+        body=json.dumps(body)
+    )
--- courses/app/routes/coursesRout.py
+++ courses/app/routes/coursesRout.py
@@ -5,12 +5,16 @@ from ..database.db import get_db
 from ..models import coursesModel
 from ..schemas import coursesSchemas 
 
+from ..rabbit.rabbitPublisher import get_rabbit_channel
+from ..rabbit.parallelsRabbitFunctions import *
+
 router = APIRouter(
     prefix="/courses",
     tags=["Courses"]
 )
 
 @router.get("/", response_model=list[coursesSchemas.Course])
-def get_courses(db: Session = Depends(get_db)):
+def get_courses(db: Session = Depends(get_db), channel: BlockingChannel = Depends(get_rabbit_channel)):
     courses = db.query(coursesModel.Course).all()
+    publishUpdatedParallel(channel=channel, course_id=20, course_name="CC", parallel_id=10)
     return courses
