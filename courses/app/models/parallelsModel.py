e7b33c39c8262a3f8aabe5e3f7dba57d18f24840
Nangel Coello <103703190+legn0@users.noreply.github.com>
2024-10-09 22:28:25 -0300

    chao name

--- courses/app/models/parallelsModel.py
+++ courses/app/models/parallelsModel.py
@@ -7,7 +7,6 @@ class Parallel(Base):
     __tablename__ = "parallels"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(255), index=True)
     course_id = Column(Integer, ForeignKey("courses.id"))
     number = Column(Integer)
     limite_cupo = Column(Integer)
