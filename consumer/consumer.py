dbc2dd9ad0112aa9afe723601ca8b4a6b9308147
Sebastian <sebastian.arrieta@usm.cl>
2024-10-17 22:52:48 -0300

    commit rabbit

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,16 +1,16 @@
-import pika
+import pika, sys, os
 import time
-
-def callback(ch, method, properties, body):
-    print(f" [x] Received {body}")
 
 def start_consumer():
     # Cambia 'localhost' por 'rabbitmq', que es el nombre del servicio en Docker Compose
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.URLParameters(f"amqp://guest:guest@localhost:5672/%2f?heartbeat=2400"))
     channel = connection.channel()
 
     # Declara la cola
     channel.queue_declare(queue='enrollment_notifications')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
 
     # Configura el consumidor
     channel.basic_consume(queue='enrollment_notifications', on_message_callback=callback, auto_ack=True)
@@ -18,5 +18,12 @@ def start_consumer():
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-if __name__ == "__main__":
-    start_consumer()
+if __name__ == '__main__':
+    try:
+        start_consumer()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
