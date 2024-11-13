d3612b8e9668f271739763b3c7226c45303c7ad8
SofyParada <sofia.parada@usm.cl>
2024-11-12 21:55:03 -0300

    NavBar Listo

--- courses/client/src/App.js
+++ courses/client/src/App.js
@@ -1,26 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import {Box} from '@chakra-ui/react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Box } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from "@chakra-ui/react";
 
-//Pages
+// Componentes
+import NavBar from './Components/NavBar'; // Asegúrate de que el `NavBar` esté exportado como `default` en su archivo
+
+// Pages
 import { VistaCursos } from './Pages/VistaCursos';
-
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='app'>
-        <div className='app__page'>
-          <Routes>
-            <Route path='/' element={<VistaCursos/>}/>
-          </Routes>
+    <ChakraProvider>
+      <BrowserRouter>
+        <div className="app">
+          {/* Incluye NavBar aquí */}
+          <NavBar />
+          <div className="app__page">
+            <Routes>
+              <Route path="/" element={<VistaCursos />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    
-    </BrowserRouter>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
