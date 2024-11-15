d92b30ca2699a45d829cfd5bc0ac794c4d0429de
ElekisDe <ncancinoh@gmail.com>
2024-11-14 22:48:39 -0300

    a

--- enrollment/frontend/src/components/EnrollmentAlumnos.js
+++ enrollment/frontend/src/components/EnrollmentAlumnos.js
@@ -11,8 +11,16 @@ const EnrollmentAlumnos = () => {
         { id: 3, nombre: "Química", codigo: "QUI103", creditos: 6, inscritos: "15/15", estado: "Pendiente", paralelos: [{ id: 5, numero: 200 }, { id: 6, numero: 201 }] },
         { id: 4, nombre: "Biología", codigo: "BIO104", creditos: 3, inscritos: "40/40", estado: "Inscrita", paralelos: [{ id: 7, numero: 202 }, { id: 8, numero: 207 }] },
         { id: 5, nombre: "Historia", codigo: "HIS105", creditos: 4, inscritos: "23/35", estado: "Pendiente",paralelos: [{ id: 9, numero: 200 }, { id: 10, numero: 201 }] },
-        { id: 6, nombre: "Historia", codigo: "HIS105", creditos: 4, inscritos: "23/35", estado: "Pendiente",paralelos: [{ id: 11, numero: 200 }, { id: 12, numero: 201 }] },
-        { id: 7, nombre: "Historia", codigo: "HIS105", creditos: 4, inscritos: "23/35", estado: "Pendiente",paralelos: [{ id: 13, numero: 200 }, { id: 14, numero: 201 }] },
+        { id: 6, nombre: "Programacion", codigo: "PRO115", creditos: 5, inscritos: "29/35", estado: "Pendiente",paralelos: [{ id: 11, numero: 200 }, { id: 12, numero: 201 }] },
+        { id: 7, nombre: "Deporte", codigo: "DEP165", creditos: 2, inscritos: "34/35", estado: "Pendiente",paralelos: [{ id: 13, numero: 200 }, { id: 14, numero: 201 }] },
+    ]);
+
+    const [ramosInscritos, setRamosInscritos] = useState([
+        { id: 8, nombre: "Matemáticas", codigo: "MAT102", creditos: 5, inscritos: "12/30", estado: "Pendiente", paralelos: [{ id: 15, numero: 200 }, { id: 16, numero: 201 }] },
+        { id: 9, nombre: "Física", codigo: "FIS103", creditos: 4, inscritos: "29/30", estado: "Inscrita", paralelos: [{ id: 17, numero: 201 }, { id: 18, numero: 204 }] },
+        { id: 10, nombre: "Química", codigo: "QUI104", creditos: 6, inscritos: "15/15", estado: "Pendiente", paralelos: [{ id: 19, numero: 200 }, { id: 20, numero: 201 }] },
+        { id: 11, nombre: "Deporte", codigo: "DEP109", creditos: 2, inscritos: "15/15", estado: "Pendiente", paralelos: [{ id: 21, numero: 200 }, { id: 22, numero: 201 }] },
+        { id: 12, nombre: "Programacion", codigo: "PRO119", creditos: 5, inscritos: "38/40", estado: "Inscrita", paralelos: [{ id: 23, numero: 200 }, { id: 24, numero: 201 }] },
     ]);
 
     const [HorariosRamo, setHorariosRamo] = useState(null);
@@ -24,7 +32,15 @@ const EnrollmentAlumnos = () => {
         {cursoId: 4, paraleloId: 7},    
         {cursoId: 5, paraleloId: 9},
         {cursoId: 6, paraleloId: 11},
-        {cursoId: 7, paraleloId: 13}
+        {cursoId: 7, paraleloId: 13},
+    ]);
+
+    const [selectedParallelsInscrito, setSelectedParallelsInscrito] = useState([
+        {cursoId: 8, paraleloId: 15},
+        {cursoId: 9, paraleloId: 17},
+        {cursoId: 10, paraleloId: 19},
+        {cursoId: 11, paraleloId: 21},
+        {cursoId: 12, paraleloId: 23}
     ]);
 
     const [Horariosdb, setHorariosdb] = useState([
@@ -42,6 +58,16 @@ const EnrollmentAlumnos = () => {
         { curso_id: 6, paralelo_id: 12, numero: 201, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
         { curso_id: 7, paralelo_id: 13, numero: 200, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
         { curso_id: 7, paralelo_id: 14, numero: 201, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 8, paralelo_id: 15, numero: 200, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 8, paralelo_id: 16, numero: 201, horario: [{ dia: "Lunes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 9, paralelo_id: 17, numero: 201, horario: [{ dia: "Miercoles", bloque: "11-12" }, { dia: "Viernes", bloque: "11-12" }] },
+        { curso_id: 9, paralelo_id: 18, numero: 204, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 10, paralelo_id: 19, numero: 200, horario: [{ dia: "Lunes", bloque: "11-12" }, { dia: "Jueves", bloque: "9-10" }] },
+        { curso_id: 10, paralelo_id: 20, numero: 201, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 11, paralelo_id: 21, numero: 200, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 11, paralelo_id: 22, numero: 201, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 12, paralelo_id: 23, numero: 200, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
+        { curso_id: 12, paralelo_id: 24, numero: 201, horario: [{ dia: "Martes", bloque: "11-12" }, { dia: "Jueves", bloque: "11-12" }] },
     ]);
 
     const handleSelectParallel = (cursoId, newParaleloId) => {
@@ -52,8 +78,16 @@ const EnrollmentAlumnos = () => {
         );
     };
 
+    const handleSelectParallelInscrito = (cursoId, newParaleloId) => {
+        setSelectedParallelsInscrito((prev) => 
+            prev.map((item) => 
+                item.cursoId === cursoId ? { ...item, paraleloId: newParaleloId } : item
+            )
+        );
+    };
+
     const handleEliminar = (cursoId) => {
-        setSelectedParallels((prev) => prev.filter((item) => item.cursoId !== cursoId));
+        setSelectedParallelsInscrito((prev) => prev.filter((item) => item.cursoId !== cursoId));
     };
     
     const handleInscribir = (ramoId) => {
@@ -139,8 +173,8 @@ const EnrollmentAlumnos = () => {
             <Box mt={8} p={5} bg="white" width="100%">
                 <Heading size="md" color="gray.800" mb={4}>Ramos Inscritos</Heading>
                 <Stack separator={<StackSeparator />} align="stretch" spacing={4} borderWidth={3} borderRadius="md" p={4} shadow="md" maxHeight="35vh" overflowY="auto">
-                    {selectedParallels.map((inscrito) => {
-                        const ramo = ramos.find((ramo) => ramo.id === inscrito.cursoId);
+                    {selectedParallelsInscrito.map((inscrito) => {
+                        const ramo = ramosInscritos.find((ramo) => ramo.id === inscrito.cursoId);
                         return (
                             <Box key={inscrito.cursoId}>
                                 <HStack spacing={4} justify="space-between" align="center">
@@ -156,7 +190,7 @@ const EnrollmentAlumnos = () => {
                                     <Text flex={1} fontSize="md" color="gray.700">
                                         Estado: {ramo.estado}
                                     </Text>
-                                    <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "orange.300", color: "white" }} onClick={() => handleSelectParallel(inscrito.cursoId, inscrito.paraleloId)}>
+                                    <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "orange.300", color: "white" }} onClick={() => handleSelectParallelInscrito(inscrito.cursoId, inscrito.paraleloId)}>
                                         Cambiar Paralelo
                                     </Button>
                                     <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "red.300", color: "white" }} onClick={() => handleEliminar(inscrito.cursoId)}>
