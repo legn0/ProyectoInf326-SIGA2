4b8a8643dc65d1b805498882adc9c03a6033eb30
nan <103703190+legn0@users.noreply.github.com>
2024-11-16 17:20:59 -0300

    enrollment client merged

--- courses/client/src/App.js
+++ courses/client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 // Pages
 import VistaCursos from './Pages/VistaCursos'; // Sin llaves, ya que es exportación por defecto
+import EnrollmentAdministrativo from './Pages/EnrollmentAdministrativo';
+import EnrollmentAlumnos from './Pages/EnrollmentAlumnos';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <div className='app__page'>
           <Routes>
             <Route path='/Cursos' element={<VistaCursos />} />
+            <Route path='/InscripcionAdmin' element={<EnrollmentAdministrativo/>}/>
+            <Route path='/InscripcionAlumno' element={<EnrollmentAlumnos/>}/>
           </Routes>
         </div>
       </div>
---
+++ courses/client/src/Pages/EnrollmentAdministrativo.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box, Button, Heading, Flex, HStack } from "@chakra-ui/react";
+
+const EnrollmentAdministrativo = ({ isAlumnosEnabled, setIsAlumnosEnabled }) => {
+  const handleInitRound = () => {
+    fetch("")
+      .catch((error) => console.error("Error en la llamada a la API:", error));
+  };
+
+  return (
+    <Box bg="gray.100" minHeight="100vh" p={4}>
+      <Heading pt={5} mb={8} color="gray.800" textAlign="center">
+        Control Administrativo
+      </Heading>
+
+      <Flex direction="column" justifyContent="center" alignItems="center" minHeight="70vh">
+        <Box p={6} width="40%" bg="white" shadow="md" mb={20} borderRadius="md" mx={4} textAlign="center" >
+          <Heading mb={4} color="gray.800" size="md">
+            Control de Inscripción
+          </Heading>
+          <HStack spacing={4} justify="center">
+            <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "green.300", color: "white" }} onClick={() => setIsAlumnosEnabled(true)}>
+              Habilitar Inscripción
+            </Button>
+            <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "red.300", color: "white" }} onClick={() => setIsAlumnosEnabled(false)}>
+              Deshabilitar Inscripción
+            </Button>
+          </HStack>
+        </Box>
+        <Box p={6} bg="white" shadow="md" borderRadius="md" mx={4} textAlign="center" width="40%">
+          <Heading mb={4} color="gray.800" size="md">
+            Acciones Adicionales
+          </Heading>
+          <HStack spacing={4} justify="center">
+            <Button variant="outline" color="gray.800" _hover={{ backgroundColor: "orange.300", color: "white" }} onClick={handleInitRound}>
+              Iniciar Ronda de Inscripción
+            </Button>
+          </HStack>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default EnrollmentAdministrativo;
