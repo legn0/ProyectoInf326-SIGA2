d95f462eec3ce45db1e70599a633ec7b30364818
Sebastian <sebastian.arrieta@usm.cl>
2024-11-27 12:07:14 -0300

    commit test

--- enrollment/tests/Enrollment_test.py
+++ enrollment/tests/Enrollment_test.py
@@ -1,39 +1,42 @@
 import unittest
 from unittest.mock import MagicMock
 import requests
-from ..app.api.routes import TestClient
-from ..app.crud.crud import EnrollmentCRUD
+#from ..app.api.routes import TestClient
+#from ..app.crud.crud import EnrollmentCRUD
 
 # Assuming your FastAPI app is called "app"
 #from app import app
 
+BASE_URL = "http://localhost:8002"
+
 class TestCreateEnrollment(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
-        self.db = MagicMock()
+    def tearDown(self):
+        if hasattr(self, "Course_Id") and hasattr(self, "Parallel_Id") and hasattr(self, "Enrollment_Id"):
+            response = requests.delete(
+                f"{BASE_URL}/api/v1/courses/{self.Course_Id}/parallels/{self.Parallel_Id}/enrollments/{self.Enrollment_Id}",
+            )
+            
+            print(response.json())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json()["message"], "Inscripción eliminada exitosamente")
 
     def test_create_enrollment_success(self):
-        # Mock the data
-        enrollment_data = {
-            "student_id": 1,
-        }
-        # Mock CRUD methods
-        EnrollmentCRUD.get_enrollment_by_student_and_course = MagicMock(return_value=None)  # No existing enrollment
-        EnrollmentCRUD.create_enrollment = MagicMock(return_value=enrollment_data)
-
         # Send the request
         response = requests.post(
-            "http://174.138.124.76/enrollment/api/v1/courses/101/parallels/5/enrollments",
+            f"{BASE_URL}/api/v1/courses/201/parallels/4/enrollments",
             json={"student_id": 1},
         )
+
+        print(response.json())
+        TestCreateEnrollment.Course_Id = response.json()['course_id']
+        TestCreateEnrollment.Parallel_Id = response.json()['parallel_id']
+        TestCreateEnrollment.Enrollment_Id = response.json()['id']
 
         # Assertions
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["is_active"], "Pendiente")
-        #EnrollmentCRUD.get_enrollment_by_student_and_course.assert_called_once_with(1, 101, 5)
-        #EnrollmentCRUD.create_enrollment.assert_called_once()
     
-    def test_create_enrollment_duplicate(self):
+    """def test_create_enrollment_duplicate(self):
         # Mock the data
         existing_enrollment = {
             "student_id": 1,
@@ -53,4 +56,4 @@ class TestCreateEnrollment(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json()["detail"], "El estudiante ya está inscrito en este curso caca")
         #EnrollmentCRUD.get_enrollment_by_student_and_course.assert_called_once_with(1, 101, 5)
-        #EnrollmentCRUD.create_enrollment.assert_not_called()
+        #EnrollmentCRUD.create_enrollment.assert_not_called()"""
